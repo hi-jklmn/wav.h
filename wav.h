9349a1b4b785e30b25124e99fa9ce2d87b31f068
jokulhaups <jokullmani12@gmail.com>
2022-03-21 15:40:54 +0000

    wav_write_data implementation

--- wav.h
+++ wav.h
@@ -58,6 +58,32 @@ typedef struct {
     uint16_t bytes_per_sample;
 } wav_fmt_desc;
 
+wav_header wav_header_from_fmt_desc(wav_fmt_desc fmt_desc) {
+    // Number of bytes of sample data
+    uint32_t data_size = fmt_desc.num_samples
+                       * fmt_desc.num_channels
+                       * fmt_desc.bytes_per_sample;
+
+    return (wav_header) {
+        .chunk_id          = {'R','I','F','F'},
+        .chunk_size        = data_size + sizeof(wav_header) - 8,
+        .format            = {'W','A','V','E'},
+        .format_chunk_id   = {'f','m','t',' '},
+        .format_chunk_size = 16,
+        .format_type       = fmt_desc.wav_format,
+        .num_channels      = fmt_desc.num_channels,
+        .sample_rate       = fmt_desc.sample_rate,
+        .byte_rate         = fmt_desc.sample_rate
+                           * fmt_desc.num_channels
+                           * fmt_desc.bytes_per_sample,
+        .block_align       = fmt_desc.bytes_per_sample
+                           * fmt_desc.num_channels,
+        .bits_per_sample   = fmt_desc.bytes_per_sample * 8,
+        .data              = {'d','a','t','a'},
+        .data_size         = data_size
+    };
+}
+
 void *wav_load_data(FILE *file, wav_fmt_desc *fmt_desc) {
     void *data = NULL;
 
@@ -73,26 +99,26 @@ void *wav_load_data(FILE *file, wav_fmt_desc *fmt_desc) {
     wav_header header;
 
     WAV_ERROR_CHECK(fread(&header, 1, 44, file) != 44,
-            "could not read WAVE header");
+                    "could not read WAVE header");
 
     WAV_ERROR_CHECK(memcmp(header.chunk_id, "RIFF", 4) != 0,
-            "RIFF 4CC Chunk ID missing");
+                    "RIFF 4CC Chunk ID missing");
 
     WAV_ERROR_CHECK(f_size - sizeof(wav_header) != header.data_size,
-            "header data size does not match file size");
+                    "header data size does not match file size");
 
     data = malloc(header.data_size);
 
     WAV_ERROR_CHECK(fread(data, 1, header.data_size, file) != header.data_size,
-            "failure reading data");
+                    "failure reading data");
 
     fmt_desc->wav_format       = header.format_type;
     fmt_desc->sample_rate      = header.sample_rate;
-    fmt_desc->bytes_per_sample = header.bits_per_sample;
-    fmt_desc->num_samples      = header.data_size
+    fmt_desc->bytes_per_sample = header.bits_per_sample / 8;
+    fmt_desc->num_channels     = header.num_channels;
+    fmt_desc->num_samples      = header.data_size 
                                / header.num_channels 
                                / fmt_desc->bytes_per_sample;
-    fmt_desc->num_channels     = header.num_channels;
 
     rewind(file);
 
@@ -100,52 +126,33 @@ void *wav_load_data(FILE *file, wav_fmt_desc *fmt_desc) {
 wav_error:
     if (data != NULL) free(data);
     if (file != NULL) rewind(file);
-    *fmt_desc = (wav_fmt_desc) { 0 };
+    *fmt_desc = (wav_fmt_desc){0};
 
     return NULL;
 }
 
-// TODO: return number of bytes successfully written?
-void wav_write_data(FILE *file, wav_fmt_desc fmt_desc, void *data) {
+size_t wav_write_data(FILE *file, wav_fmt_desc fmt_desc, void *data) {
     WAV_ERROR_CHECK(file == NULL, "file is NULL");
 
-    // Number of bytes of sample data
-    uint32_t data_size = fmt_desc.num_samples
-                       * fmt_desc.num_channels
-                       * fmt_desc.bytes_per_sample;
+    wav_header header = wav_header_from_fmt_desc(fmt_desc);
 
-    wav_header header = {
-        .chunk_id          = {'R','I','F','F'},
-        .chunk_size        = data_size + sizeof(wav_header) - 8,
-        .format            = {'W','A','V','E'},
-        .format_chunk_id   = {'f','m','t',' '},
-        .format_chunk_size = 16,
-        .format_type       = fmt_desc.wav_format,
-        .num_channels      = fmt_desc.num_channels,
-        .sample_rate       = fmt_desc.sample_rate,
-        .byte_rate         = fmt_desc.sample_rate
-                           * fmt_desc.num_channels
-                           * fmt_desc.bytes_per_sample,
-        .block_align       = fmt_desc.bytes_per_sample
-                           * fmt_desc.num_channels,
-        .bits_per_sample   = fmt_desc.bytes_per_sample / 8,
-        .data              = {'d','a','t','a'},
-        .data_size         = data_size
-    };
+    size_t bytes_written = 0;
 
-    fwrite(&header, sizeof(wav_header), 1, file);
+    bytes_written += fwrite(&header, 1, sizeof(wav_header), file);
 
-    //WAV_ERROR_CHECK(!ferror(file), "Error writing WAVE header");
+    WAV_ERROR_CHECK(bytes_written != 44,
+                    "Writing header data results in unexpected fwrite return");
 
-    fwrite(data, 
-           fmt_desc.bytes_per_sample, 
-           fmt_desc.num_samples * fmt_desc.num_channels, 
-           file);
+    WAV_ERROR_CHECK(ferror(file), "Error writing WAVE header");
 
-    //WAV_ERROR_CHECK(!ferror(file), "Error writing WAVE data");
+    bytes_written += fwrite(data, 1, header.data_size, file);
 
+    WAV_ERROR_CHECK(bytes_written != header.data_size + 44,
+                    "Writing WAVE data results in unexpected fwrite return");
+
+    WAV_ERROR_CHECK(ferror(file), "Error writing WAVE data");
 wav_error:
-    return;
+    return bytes_written;
 }
 
 #endif
