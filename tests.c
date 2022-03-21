bc72992ad8016c647df37b2fb1d6986919d0622b
jokulhaups <jokullmani12@gmail.com>
2022-03-21 15:36:16 +0000

    clang-format and more correct fIO api usage

--- tests.c
+++ tests.c
@@ -1,19 +1,19 @@
-#include "wav.h"
 #include <assert.h>
 
-#define TEST(expr) printf("[%s] "#expr"\n", expr ? "PASSED" : "FAILED" );
+#include "wav.h"
+
+#define TEST(expr) printf("[%s] " #expr "\n", expr ? "PASSED" : "FAILED");
 
 #define TEST_FILE "/usr/share/sounds/alsa/Noise.wav"
 
 int main() {
-
     FILE *file;
 
     printf("-- TESTING: Loading Noise.wav ----------------------------\n");
 
     TEST(sizeof(wav_header) == 44);
 
-    file = fopen(TEST_FILE, "r");
+    file = fopen(TEST_FILE, "rb");
 
     wav_fmt_desc fmt_desc;
     void *data = wav_load_data(file, &fmt_desc);
@@ -24,11 +24,13 @@ int main() {
 
     printf("-- TESTING: Writing out.wav ----------------------------\n");
 
-    file = fopen("./out.wav", "w");
+    file = fopen("./out.wav", "wb");
 
     wav_write_data(file, fmt_desc, data);
 
-    TEST(system("diff "TEST_FILE" out.wav") == 0);
+    fclose(file);
+
+    TEST(system("diff " TEST_FILE " out.wav") == 0);
 
     return 0;
 }
